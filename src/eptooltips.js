f4f07fbaba84968d47005b2086d83c83f08b4014
Carl Grundberg <carl.grundberg@menmo.se>
2014-06-23 22:13:25 +0200

    Updated template

--- src/eptooltips.js
+++ src/eptooltips.js
@@ -4,6 +4,13 @@ var playerLinkPattern = /http:\/\/www\.eliteprospects\.com\/player\.php\?player=
 // Endpoint to get data about the player, [playerId] is replaced with the actual id.
 var playerApiEndpoint = 'http://api.eliteprospects.com/beta/players/[playerId]';
 
+var capitalize = function(s) {
+    if(s) {
+        s = s.charAt(0).toUpperCase() + s.slice(1).toLowerCase();
+    }
+    return s;
+};
+
 // Override setContent since we fetch JSON instead of presentation ready HTML.
 // Convert the JSON into HTML with a handlebars template.
 Opentip.prototype.setContent = function(content) {
@@ -11,6 +18,10 @@ Opentip.prototype.setContent = function(content) {
         var template = Handlebars.getTemplate('player');
         var player = JSON.parse(content).data;
         player.isActive = player.playerStatus == 'ACTIVE';
+        player.status = capitalize(player.playerStatus);
+        player.isPlayer = player.playerPosition != 'GOALIE';
+        player.position = capitalize(player.playerPosition);
+        player.shoots = capitalize(player.shoots);
         this.content = template(player);
     }
     this._newContent = true;
